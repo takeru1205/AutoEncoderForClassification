20377879514afa480863010b2bb2df4f9f648f55
takeru1205 <takeru0639418@gmail.com>
2021-04-10 07:49:55 +0900

    [update] transform

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@
 Train and Evaluation Script of Combined Model AutoEncoder and Classifier.
 These are trained separatly.
 """
-import argparse
+import argparse, itertools
 import numpy as np
 import torch
 import torchvision
@@ -16,8 +16,8 @@ from sklearn.metrics import confusion_matrix, f1_score, classification_report
 from sklearn.model_selection import train_test_split
 
 from load_data import ImbalancedCIFAR10
-from model import CAE, CAE2, CAE3, CAE4
-from utils import GridMask, AddNoise, imshow, imsave, ImbalancedDatasetSampler
+from model import CAE3
+from utils import GridMask, AddNoise, imshow, imsave, ImbalancedDatasetSampler, calc_img_stats
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--train-epoch", default=50, type=int, help='Epoch of train Classification')
@@ -27,40 +27,15 @@ args = parser.parse_args()
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-transform_mean = (0.4735, 0.4640, 0.4295)
-transform_std = (0.2200, 0.2165, 0.2151)
-
-ae_transform = transforms.Compose(
-        [
-            # transforms.Grayscale(),
-            transforms.RandomHorizontalFlip(),
-            # transforms.RandomVerticalFlip(),
-            # transforms.RandomAffine(degrees=[-10, 10], translate=(0.1, 0.1), scale=(0.5, 1.5)),
-            transforms.RandomRotation(10),
-            # GridMask(p=0.3),
-            # AddNoise(),
-            transforms.ToTensor(),
-            transforms.Normalize(transform_mean, transform_std)
-        ])
-
+# get from calc_img_stats
+transform_mean = [0.4920, 0.4825, 0.4500]
+transform_std = [0.2039, 0.2009, 0.2026]
 
 train_transform = transforms.Compose(
         [
-            # transforms.Grayscale(),
             transforms.RandomHorizontalFlip(),
-            # transforms.RandomVerticalFlip(),
-            # transforms.RandomAffine(degrees=[-10, 10], translate=(0.1, 0.1), scale=(0.5, 1.5)),
-            transforms.RandomRotation(degrees=10),
-            # GridMask(p=0.3),
-            # AddNoise(p=0.6),
-            transforms.ToTensor(),
-            transforms.Normalize(transform_mean, transform_std)
-        ])
-
-
-ae_evaluate_transform = transforms.Compose(
-        [
-            # transforms.Grayscale(),
+            transforms.RandomRotation(degrees=20),
+            transforms.RandomResizedCrop(32),
             transforms.ToTensor(),
             transforms.Normalize(transform_mean, transform_std)
         ])
@@ -68,7 +43,6 @@ ae_evaluate_transform = transforms.Compose(
 
 evaluate_transform = transforms.Compose(
         [
-            # transforms.Grayscale(),
             transforms.ToTensor(),
             transforms.Normalize(transform_mean, transform_std)
         ])
@@ -82,25 +56,26 @@ torch.cuda.manual_seed(SEED)
 
 
 # Log
-writer = SummaryWriter(log_dir=f'sample/cae3-{args.ae_epoch}-{args.train_epoch}-convclassify-fulltransform-weightloss-convpool-nonoise-novert-nomask')
+writer = SummaryWriter(log_dir=f'sample/cae3-{args.ae_epoch}-{args.train_epoch}-convclassify-decdp-rndcrop-bn-avgpool-xnorm-2dp02-128batch-stats')
 
 # Load Train Data
 train_imbalance_class_ratio = np.array([1., 1., .5, 1., .5, 1., 1., 1., 1., .5])
 # train_imbalance_class_ratio = np.array([1.] * 10)
 # train_imbalance_class_ratio = np.array([.5] * 10)
 train_set = ImbalancedCIFAR10(train_imbalance_class_ratio, transform=evaluate_transform)
-#train_loader = torch.utils.data.DataLoader(train_set, batch_size=8, shuffle=True, num_workers=4)
 
 # Cross Validation Dataset(Stratified K Fold)
 train_indices, val_indices = train_test_split(list(range(len(train_set.labels))), test_size=0.2, stratify=train_set.labels)
 train_dataset = Subset(train_set, train_indices)
-train_dataset.dataset.transform = ae_transform
+train_dataset.dataset.transform = train_transform
 val_dataset = Subset(train_set, val_indices)
-val_dataset.dataset.transform = ae_evaluate_transform
+val_dataset.dataset.transform = evaluate_transform
 
 train_loader = DataLoader(train_dataset, sampler=ImbalancedDatasetSampler(train_dataset),
-                          batch_size=64, shuffle=False, num_workers=4)
-val_loader = DataLoader(val_dataset, batch_size=16, shuffle=True, num_workers=4)
+                           batch_size=128, shuffle=False, num_workers=4)
+# tmp_train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True, num_workers=4)
 
 print(f'Train Size: {len(train_loader.dataset)}')
 print(f'Validation Size: {len(val_loader.dataset)}')
@@ -124,18 +99,15 @@ if args.load:
     net.decoder.load_state_dict(torch.load('model_weights/Decoder3'))
 net.train()
 
-_, class_size = torch.tensor(train_dataset.dataset.labels[train_indices]).unique(return_counts=True)
-weight = class_size.to(torch.float) / torch.max(class_size).to(torch.float)
-criterion = nn.CrossEntropyLoss(weight=weight.to(device))
+criterion = nn.CrossEntropyLoss()
 ae_criterion = nn.MSELoss()
-# ae_criterion = nn.BCELoss()
-ae_optimizer = optim.Adam(net.parameters(), lr=0.0005, weight_decay=1e-5)
+# ae_optimizer = optim.Adam(net.parameters(), lr=0.0005, weight_decay=1e-5)
+ae_optimizer = optim.AdamW(itertools.chain(net.encoder.parameters(), net.decoder.parameters()), lr=0.0005, betas=(0.9, 0.999))
 # ae_optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-5)
-optimizer = optim.Adam(net.parameters(), lr=0.0005, betas=(0.9, 0.999))
-# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.AdamW(itertools.chain(net.encoder.parameters(), net.classifier.parameters()), lr=0.0005, betas=(0.9, 0.999))
 
 # Train Auto Encoder Part
-print('Start Auto Encoder Training')
+# print('Start Auto Encoder Training')
 ae_iter = 0
 for epoch in range(args.ae_epoch):
     if args.load:
@@ -159,10 +131,10 @@ for epoch in range(args.ae_epoch):
             running_loss = 0.0
 
 # Test Auto Encoder Output
-dataiter = iter(val_loader)
-images, labels = dataiter.next()
+# dataiter = iter(val_loader)
+# images, labels = dataiter.next()
 # imsave(torchvision.utils.make_grid(images), 'target.png')
-output = net(images.to(device))
+# output = net(images.to(device))
 # imsave(torchvision.utils.make_grid(output.cpu().data), 'predict.png')
 
 # Train Classification Part
@@ -193,11 +165,13 @@ for epoch in range(args.train_epoch):
         train_iter += 1
 
         if i % 100 == 99:
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+            print('[%d, %5d] Train Loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
             writer.add_scalar('Loss/Train', running_loss, train_iter)
             running_loss = 0.0
+
     # Validation
     total, correct = 0, 0
+    val_loss = 0.0
     for i, data in enumerate(val_loader, 0):
         inputs, labels = data
         inputs = inputs.to(device)
@@ -205,13 +179,17 @@ for epoch in range(args.train_epoch):
         predicted = net.classify(inputs)
         loss = criterion(predicted, labels)
 
+        val_loss += loss.item()
+
         _, predicted = torch.max(predicted.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
         val_iter += 1
-        if val_iter % 10 == 0:
+        if val_iter % 100 == 99:
             writer.add_scalar('Validation/Loss', loss.item(), val_iter)
+            print('[%d, %5d] Valid Loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+            val_loss = 0.0
         
     writer.add_scalar('Validation/Score', (100 * correct / total), epoch)
 
@@ -230,7 +208,7 @@ correct = 0
 total = 0
 predicted_list = []
 target_list = []
-# 勾配を記憶せず（学習せずに）に計算を行う
+
 with torch.no_grad():
     for data in test_loader:
         images, labels = data
